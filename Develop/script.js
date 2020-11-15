70a66244ea393c27914053d989ca135f929eb624
Erik Portillo <Erik.Portillo@dcusa.tentamus.com>
2020-11-14 19:48:30 -0800

    Automatically runs through the process of building the password

--- Develop/script.js
+++ Develop/script.js
@@ -19,21 +19,38 @@ generateBtn.addEventListener("click", writePassword);
 
 
 var userOptions = ["abcdefghijklmnopqrstuvwxyz","ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
-var userPassword = ''
+var userPasswordpossibles = ''
+var userPassworddisplayed = ''
 
+var passwordLength = prompt("How many digits (please enter a number from 8 to 128)?")
+
+//check if the desired length is in set parameters
+if (passwordLength < 8 || passwordLength > 128) {
+  for (passwordLength; passwordLength < 8 || passwordLength > 128; passwordLength = prompt("Your password must be a number from 8 to 128. Please try again: how long do you want your password to be?")) {    
+  }
+}
+
+//confirm each question regarding what characters to use in password
 var isUpperscase = confirm("Do you want to inlcude Upper-Case letters?")
 var isNumbers = confirm("Do you want to inlcude Numbers?")
 var isSpecials = confirm("Do you want to inlcude special characters?")
 
 var userQueries = [true, isUpperscase, isNumbers, isSpecials]
 
-//turn this into a function
+//loops through each boolean as to whether or not to include that same index within the available sets of chars
 for (var i = 0; i < userQueries.length; i++) {
   if (userQueries[i]) {
-    userPassword = userPassword + userOptions[i]
+    userPasswordpossibles = userPasswordpossibles + userOptions[i]
   }
 }
 
-alert(userPassword)
+console.log(userPasswordpossibles)
 
+//creates the password. Random number must be multiplied by the length of the string of possible chars, then rounded to a whole number, then appended to the final password.
+//turn this into a function
+for (var i = 0; i < passwordLength; i++) {
+  var newPasswordchar = userPasswordpossibles.charAt(Math.round(Math.random() * userPasswordpossibles.length))
+  userPassworddisplayed = userPassworddisplayed + newPasswordchar
+}
 
+console.log(userPassworddisplayed)
