96ca2bb6db35917c56d065274948c67162e67d3d
Erik Portillo <Erik.Portillo@dcusa.tentamus.com>
2020-11-14 19:17:50 -0800

    builds string of user desired characters

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,28 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//
+
+
+
+
+var userOptions = ["abcdefghijklmnopqrstuvwxyz","ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"]
+var userPassword = ''
+
+var isUpperscase = confirm("Do you want to inlcude Upper-Case letters?")
+var isNumbers = confirm("Do you want to inlcude Numbers?")
+var isSpecials = confirm("Do you want to inlcude special characters?")
+
+var userQueries = [true, isUpperscase, isNumbers, isSpecials]
+
+//turn this into a function
+for (var i = 0; i < userQueries.length; i++) {
+  if (userQueries[i]) {
+    userPassword = userPassword + userOptions[i]
+  }
+}
+
+alert(userPassword)
+
+
